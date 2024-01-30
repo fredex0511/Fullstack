918fa1daa67388f9a236201b488a3ff8b854177d
Cholico <elcholobrizuela@gmail.com>
2024-01-29 22:28:21 -0600

    Modificaciones en el diseño de store y update

--- practicajwt/resources/views/Juegos/store.blade.php
+++ practicajwt/resources/views/Juegos/store.blade.php
@@ -1,43 +1,45 @@
 @extends('/Juegos/template')
 
-<head>
-    @section('title', 'Index')
-</head>
+@section('title', 'Update')
+
 @section('content')
-    <form action="{{ route('juegos.store') }}" method="POST">
-    @csrf
+<div class="card" style="width: 18rem; margin: 0 auto; float: none; margin-bottom: 10px;">
+    <div class="card-body">
+        <h5 class="card-title">Actualizar Juego</h5>
+        <form action="{{ route('juegos.store') }}" method="POST">
+        @csrf
+        @method('POST')
 
-
-    <div class="mb-3">
-        <label for="Nombre" class="form-label">Nombre del Juego</label>
-        <input type="text" class="form-control" id="Nombre" name="nombre">
-        @error('nombre')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+        <div class="mb-3">
+            <label for="Nombre" class="form-label fs-6">Nombre del Juego</label>
+            <input type="text" class="form-control" id="Nombre" name="nombre" ">
+            @error('nombre')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="Description" class="form-label fs-6">Descripcion</label>
+            <input type="text" class="form-control" id="Description" name="description">
+            @error('description')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="Precio" class="form-label fs-6">Precio</label>
+            <input type="text" class="form-control" id="Precio" name="precio">
+            @error('precio')
+            <div class="alert alert-danger fs-6">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="Desarrolador" class="form-label fs-6">Desarrolador</label>
+            <input type="text" class="form-control" id="Desarrolador" name="desarrollador">
+            @error('desarrollador')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Agregar</button>
+        </form>
     </div>
-    
-    <div class="mb-3">
-        <label for="Description" class="form-label">Descripcion</label>
-        <input type="text" class="form-control" id="Description" name="description">
-        @error('description')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    </div>
-    <div class="mb-3">
-        <label for="Precio" class="form-label">Precio</label>
-        <input type="text" class="form-control" id="Precio" name="precio">
-        @error('precio')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    </div>
-    <div class="mb-3">
-        <label for="Desarrollador" class="form-label">Desarrollador</label>
-        <input type="text" class="form-control" id="Desarrollador" name="desarrollador">
-        @error('desarrollador')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    </div>
-    
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+</div>
 @endsection
--- practicajwt/resources/views/Juegos/update.blade.php
+++ practicajwt/resources/views/Juegos/update.blade.php
@@ -3,43 +3,47 @@
 @section('title', 'Update')
 
 @section('content')
-    <form action="{{ route('juegos.update', $juego) }}" method="POST">
-    @csrf
-    @method('PUT')
+<div class="card" style="width: 18rem; margin: 0 auto; float: none; margin-bottom: 10px;">
+    <div class="card-body">
+        <h5 class="card-title">Actualizar Juego</h5>
+        <form action="{{ route('juegos.update', $juego) }}" method="POST">
+        @csrf
+        @method('PUT')
 
-
-    <div class="mb-3">
-        <label for="Id" class="form-label">Id del juego</label>
-        <input type="text" class="form-control" id="ID" name="id" value="{{ $juego->id }}" readonly>
+        <div class="mb-3">
+            <label for="Id" class="form-label fs-6">Id del juego</label>
+            <input type="text" class="form-control" id="ID" name="id" value="{{ $juego->id }}" readonly>
+        </div>
+        <div class="mb-3">
+            <label for="Nombre" class="form-label fs-6">Nombre del Juego</label>
+            <input type="text" class="form-control" id="Nombre" name="nombre" value="{{ $juego->nombre }}">
+            @error('nombre')
+            <div class="alert alert-danger fs-6">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="Description" class="form-label fs-6">Descripcion</label>
+            <input type="text" class="form-control" id="Description" name="description" value="{{ $juego->description }}">
+            @error('description')
+            <div class="alert alert-danger fs-6">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="Precio" class="form-label fs-6">Precio</label>
+            <input type="text" class="form-control" id="Precio" name="precio" value="{{ $juego->precio }}">
+            @error('precio')
+            <div class="alert alert-danger fs-6">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="Desarrollador" class="form-label fs-6">Desarrollador</label>
+            <input type="text" class="form-control" id="Desarrollador" name="desarrollador" value="{{ $juego->desarrollador }}">
+            @error('desarrollador')
+            <div class="alert alert-danger fs-6">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Actualizar</button>
+        </form>
     </div>
-    <div class="mb-3">
-        <label for="Nombre" class="form-label">Nombre del Juego</label>
-        <input type="text" class="form-control" id="Nombre" name="nombre" value="{{ $juego->nombre }}">
-        @error('nombre')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-    <div class="mb-3">
-        <label for="Description" class="form-label">Descripcion</label>
-        <input type="text" class="form-control" id="Description" name="description" value="{{ $juego->description }}">
-        @error('description')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-    <div class="mb-3">
-        <label for="Precio" class="form-label">Precio</label>
-        <input type="text" class="form-control" id="Precio" name="precio" value="{{ $juego->precio }}">
-        @error('precio')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-    <div class="mb-3">
-        <label for="Desarrollador" class="form-label">Desarrollador</label>
-        <input type="text" class="form-control" id="Desarrollador" name="desarrollador" value="{{ $juego->desarrollador }}">
-        @error('desarrollador')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+</div>
 @endsection
